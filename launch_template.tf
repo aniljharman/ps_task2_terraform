078a9ccf88d744b6909dc21e7a5fe19f3b64719e
aniljharman <anil.jose@harman.com>
2025-07-05 11:38:52 +0530

    initial commit

--- launch_template.tf
+++ launch_template.tf
@@ -1,10 +1,11 @@
 data "aws_ami" "al2" {
   most_recent = true
   owners      = ["amazon"]
+
   filter {
-    name = "name"
+    name   = "name"
     values = ["amzn2-ami-kernel-*-x86_64-gp2"]
-    }
+  }
 }
 
 resource "aws_launch_template" "web" {
@@ -13,16 +14,32 @@ resource "aws_launch_template" "web" {
   instance_type = var.instance_type
 
   user_data = base64encode(<<-EOS
-    #!/bin/bash
-    yum update -y
-    yum install -y nginx
-    sed -i 's/listen       80;/listen       8080;/' /etc/nginx/nginx.conf
-    echo "<h1>Zantac POC $(hostname)</h1>" > /usr/share/nginx/html/index.html
-    systemctl enable nginx && systemctl start nginx
-  EOS
+              #!/bin/bash
+              set -e
+
+              yum update -y
+              amazon-linux-extras enable nginx1
+              amazon-linux-extras install -y nginx1
+              
+              sed -i 's/listen       80;/listen       8080;/' /etc/nginx/nginx.conf
+              
+              echo "<h1>Zantac POC - $(hostname)</h1>" > /usr/share/nginx/html/index.html
+
+              systemctl enable nginx
+              systemctl restart nginx
+              EOS
   )
 
   network_interfaces {
-    security_groups = [aws_security_group.web_sg.id]
+    associate_public_ip_address = true
+    security_groups             = [aws_security_group.web_sg.id]
+  }
+
+  tag_specifications {
+    resource_type = "instance"
+
+    tags = {
+      Name = "poc-web"
+    }
   }
 }
